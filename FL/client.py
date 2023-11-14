85ffab53172001ead3ead80a26b731710e6d57b5
electroblats <adam.laurell2@gmail.com>
2023-11-14 16:04:01 +0100

    1

--- FL/client.py
+++ FL/client.py
@@ -72,3 +72,25 @@ class Net(nn.Module):
 # Load model and data
 net = Net().to(DEVICE)
 trainloader, testloader, num_examples = load_data()
+
+class CifarClient(fl.client.NumPyClient):
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        params_dict = zip(net.state_dict().keys(), parameters)
+        state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+        net.load_state_dict(state_dict, strict=True)
+
+    def fit(self, parameters, config):
+        self.set_parameters(parameters)
+        train(net, trainloader, epochs=1)
+        return self.get_parameters(config={}), num_examples["trainset"], {}
+
+    def evaluate(self, parameters, config):
+        self.set_parameters(parameters)
+        loss, accuracy = test(net, testloader)
+        return float(loss), num_examples["testset"], {"accuracy": float(accuracy)}
+    
+fl.client.start_numpy_client(server_address="[::]:8080", client=CifarClient())
+
