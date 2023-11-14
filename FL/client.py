ebf29f2d17ccc3fa8bf16b80bc03b6e0b0febd0c
electroblats <adam.laurell2@gmail.com>
2023-11-14 15:28:15 +0100

    1

---
+++ FL/client.py
@@ -0,0 +1,10 @@
+from collections import OrderedDict
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torchvision.datasets import CIFAR10
+
+import flwr as fl
