c66218025d8c9a23ba7076c13ea168a006e91fdb
Slipstream07 <Jesper.V.Vestin@gmail.com>
2023-11-20 11:46:29 +0100

    fixes

--- FL/server.py
+++ FL/server.py
@@ -1,3 +1,14 @@
 import flwr as fl
 
-fl.server.start_server(config=fl.server.ServerConfig(num_rounds=3))
+def weighted_average(metrics):
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+    return{"accuracy": sum(accuracies) / sum(examples)}
+
+fl.server.start_server(
+    server_address="0.0.0.0:8080",
+    config=fl.server.ServerConfig(num_rounds=3),
+    strategy=fl.server.strategy.FedAvg(
+        evaluate_metrics_aggregation_fn=weighted_average,
+    ),
+)
