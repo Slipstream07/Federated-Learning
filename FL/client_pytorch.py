b1e4c152929bad811c3f61a7c8ca201ecf00a5d7
Slipstream07 <Jesper.V.Vestin@gmail.com>
2023-12-18 12:04:06 +0100

    fixes

--- FL/client_pytorch.py
+++ FL/client_pytorch.py
@@ -5,6 +5,7 @@ from collections import OrderedDict
 import flwr as fl
 import torch
 import torch.nn as nn
+import tenseal as ts
 import torch.nn.functional as F
 from torch.utils.data import DataLoader, random_split
 from torchvision.datasets import CIFAR10, MNIST
@@ -133,6 +134,31 @@ def prepare_dataset(use_mnist: bool):
         val_partitions.append(for_val)
 
     return train_partitions, val_partitions, testset
+
+def encryption(params):
+    context = ts.Context()
+    context.generate_galois_keys()
+    
+    # Encrypt each parameter using a TenSEAL CKKS encoder
+    encrypted_params = [ts.ckks_vector(context, p.tolist()) for p in params]
+    
+    # Serialize encrypted parameters
+    encrypted_params_serialized = [ep.serialize() for ep in encrypted_params]
+    
+    return encrypted_params_serialized
+
+def decryption(encrypted_params_serialized):
+    # Initialize TenSEAL context and keys
+    context = ts.Context()
+    context.generate_galois_keys()
+    
+    # Deserialize encrypted parameters
+    encrypted_params = [ts.ckks_vector_from(context, ep) for ep in encrypted_params_serialized]
+    
+    # Decrypt each parameter using a TenSEAL CKKS encoder
+    decrypted_params = [ep.decrypt() for ep in encrypted_params]
+    
+    return decrypted_params
 
 
 # Flower client, adapted from Pytorch quickstart/simulation example
