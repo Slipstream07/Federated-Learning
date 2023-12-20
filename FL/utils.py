bba179e88c8671b92e939b174adcc31759e30e76
Slipstream07 <Jesper.V.Vestin@gmail.com>
2023-12-20 14:32:29 +0100

    fix

--- FL/utils.py
+++ FL/utils.py
@@ -8,7 +8,7 @@ def write_data(file_name: str, data: bytes):
         f.write(data)
 
 def read_data(file_name: str) -> bytes:
-    with open(file_name, 'rb') as f:
+    with open(file_name, "rb") as f:
         data = f.read()
     #base64 to bytes
     return base64.b64decode(data)
